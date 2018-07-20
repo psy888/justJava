38b3c3cd5553ef37e759dcf8d24981d004f2526e
psy888 <lpsyl888l@yandex.ru>
2018-07-20 12:44:18 +0300

    remove display method add intent

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.android.justjava;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -20,11 +23,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    int quantity = 1;
+
     /**
      * This method is called when the order button is clicked.
      */
 
-    int quantity = 0;
+
 
     public void submitOrder(View view) {
         // Имя клиента
@@ -37,10 +42,9 @@ public class MainActivity extends AppCompatActivity {
         CheckBox chokolate = findViewById(R.id.chokolateCheckBox);
         boolean hasChokolate = chokolate.isChecked();
         //Price method
-        int price = calculatePrice();
-
+        int price = calculatePrice(hasSlivki, hasChokolate);
         //Final Message
-        displayMessage(createOrderSummery(price, hasSlivki, hasChokolate, name));
+        createOrderSummery(price, hasSlivki, hasChokolate, name);
 
         //Toast message
         Toast toast = Toast.makeText(this, "Заказ обработан!", Toast.LENGTH_SHORT);
@@ -50,18 +54,35 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This Method decrement quantity
      */
+
     public void decrement(View view) {
+        Button decrement = findViewById(R.id.decrement);
+        Button increment = findViewById(R.id.increment);
+        if (quantity == 1) {
+            decrement.setEnabled(false);
+            Toast.makeText(this, "Заказ меньше одной чашки не возможен!", Toast.LENGTH_SHORT).show();
+            return;
+        } else {
+            increment.setEnabled(true);
+        }
         display(--quantity);
-        //int price = calculatePrice();
-        displayMessage(String.valueOf(calculatePrice()));
     }
 
     /**
      * This Method increment quantity
      */
+
     public void increment(View view) {
+        Button decrement = findViewById(R.id.decrement);
+        Button increment = findViewById(R.id.increment);
+        if (quantity == 100) {
+            increment.setEnabled(false);
+            Toast.makeText(this, "Превышен лимит заказа.", Toast.LENGTH_SHORT).show();
+            return;
+        } else {
+            decrement.setEnabled(true);
+        }
         display(++quantity);
-        displayMessage(String.valueOf(calculatePrice()));
     }
     /**
      * ----- промежуточная цена-----
@@ -75,22 +96,22 @@ public class MainActivity extends AppCompatActivity {
         quantityTextView.setText(" " + number);
     }
 
-    /**
-     * This method displays the given price on the screen.
-     */
-
-    private void displayMessage(String message) {
-        TextView orderSummaryTextView = findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView.setText(message);
-    }
 
     /**
      * Просчет цены заказа
-     *
+     * @param slivki - добавлять сливки
+     * @param chokolate - добавлять шоколад
      * @return количество * цену
      */
-    private int calculatePrice() {
-        return 5 * quantity;
+    private int calculatePrice(boolean slivki, boolean chokolate) {
+        int price = 5;
+        if (slivki) {
+            price += 1;
+        }
+        if (chokolate) {
+            price += 2;
+        }
+        return price * quantity;
     }
 
     /**
@@ -98,10 +119,20 @@ public class MainActivity extends AppCompatActivity {
      * @param topSlivki    -  Добавлять сливки
      * @param topChokolate - добавлять шоколад
      * @param name - имя клиента
-     * @return - возвращает текст заказа с входящими параметрами
      */
-    private String createOrderSummery(int total, boolean topSlivki, boolean topChokolate, String name) {
-        return "Имя: " + name + "\nДобавить Взбитые сливки? :" + topSlivki + "\nДобавить шоколад? :" + topChokolate + "\nКоличество: " + quantity + " \nИтого: " + total + "грн.\nСпасибо за покупку!";
+    private void createOrderSummery(int total, boolean topSlivki, boolean topChokolate, String name) {
+        String[] addresses = {"mail@mail.com"};
+        String order = "Имя: " + name + "\nДобавить Взбитые сливки? :" + topSlivki + "\nДобавить шоколад? :" + topChokolate + "\nКоличество: " + quantity + " \nИтого: " + total + "грн.\nСпасибо за покупку!";
+
+        //Create Intent
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); //только для email приложений
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Just Java заказ для " + name);
+        intent.putExtra(Intent.EXTRA_TEXT, order);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
 }
